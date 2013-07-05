5bc781cbcb07faad225741ddcbeb9c221810913c
Niko Felger <niko.felger@gmail.com>
2013-07-05 16:30:51 +0200

    Initial commit: pulling data from sprintly API.

---
+++ get-tickets-of-the-day.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'json'
+require 'sequel'
+
+DB = Sequel.sqlite('items.db')
+DB.create_table?(:items) do
+  String :day
+  Integer :item_id
+  String :status
+  String :size
+  String :item_type
+end
+db = DB[:items]
+
+key  = ENV['SPRINTLY_API_KEY']
+user = ENV['SPRINTLY_USER']
+product = ENV['SPRINTLY_PRODUCT_ID']
+
+offset = 0
+limit  = 100
+items  = []
+previous_size = -1
+while (previous_size != items.size)
+  previous_size = items.size
+  cmd = "curl -s -u #{user}:#{key} 'https://sprint.ly/api/products/#{product}/items.json?status=backlog,in-progress,completed,accepted&limit=#{limit}&offset=#{offset}'"
+  puts cmd
+  response = `#{cmd}`
+  items += JSON.parse(response)
+  offset += limit
+end
+
+items.each do |item|
+  day       = Date.today.iso8601
+  item_id   = item['number']
+  status    = item['status']
+  size      = item['score']
+  item_type = item['type']
+
+  unless db.where(:day => day, :item_id => item_id).any?
+    DB[:items].insert(:day => day, :item_id => item_id, :status => status, :size => size, :item_type => item_type)
+  end
+end
