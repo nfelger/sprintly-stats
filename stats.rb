f2c8d3c9c0d2785fd7806dcbd57c93cea75a36e9
Niko Felger <niko.felger@gmail.com>
2013-07-09 19:28:17 +0200

    Tag filtering: exclude items that don't match the tag, not the other way round (d'oh).

--- stats.rb
+++ stats.rb
@@ -18,7 +18,7 @@ db = Sequel.sqlite('items.db')[:items]
 items_by_day = {}
 
 db.all.each do |item|
-  next if tag && JSON.parse(item[:tags]).include?(tag)
+  next if tag && !(JSON.parse(item[:tags]).include?(tag))
   items_by_day[item[:day]] ||= Hash.new(0)
   items_by_day[item[:day]][item[:status]] += SIZES[item[:size]]
 end
